c9d03bec9eaee2bed62d3b9c3db7dd89fb549771
Mahmoud Khattab <maymoo99@hotmail.com>
2018-03-08 20:33:05 -0700

    TLB code skeleton

--- assignment2.c
+++ assignment2.c
@@ -4,13 +4,26 @@
 #define BUFFER_SIZE 10
 
 #define OFFSET_MASK 255
-#define PAGES 128 // since physical address space is 1bit smaller
+#define PAGES 128 // since physical address space is 1bit smaller (is this right?)
 #define TLB_SIZE 16 // pages held in TLB
 #define OFFSET_BITS 8
 #define PAGE_SIZE 256
 
+typedef struct TLBentry {
+	int page;
+	int frame;
+}TLBentry;
+
+void search_TLB(){} //ADD CODE
+
+void TLB_Add(){} //ADD CODE
+
+void TLB_Update(){} //ADD CODE
+
 int main(int argc, char* argv[]){
-	int page_table[PAGES] = {6,4,3,7,0,1,2,5};
+	//int page_table[PAGES] = {6,4,3,7,0,1,2,5};
+
+	TLBentry TLB[TLB_SIZE];
 
 	FILE *fptr = fopen("addresses.txt", "r");
 	char buff[BUFFER_SIZE];
@@ -23,8 +36,8 @@ int main(int argc, char* argv[]){
 		int page_num = virt_addr >> OFFSET_BITS;
 		int page_off = virt_addr & OFFSET_MASK;
 
-		printf("Virtual addr is %d: Page# = %d & Offset = %d.\n", 
-											virt_addr, page_num, page_off);
+		//printf("Virtual addr is %d: Page# = %d & Offset = %d.\n", 
+		//									virt_addr, page_num, page_off);
 
 		// int frame_num = page_table[page_num];
 
